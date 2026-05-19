b6c5c039ca5ac367cc337ff25e829555d58e7eec
Zhafir Avel <167537812+zhafiravel@users.noreply.github.com>
2026-05-20 06:19:51 +0700

    testing actions in Counter.java
    Fixed package declaration by removing semicolon.

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,4 +1,4 @@
-package src.main.java;//ini aslinya ada ; tapi buat tugas, kita coba rusakin dulu
+package src.main.java//ini aslinya ada ; tapi buat tugas, kita coba rusakin dulu
 
 public class Counter {
 
